3296c202a8781ff2941143dd22f26df6e02a0ae7
Valeria Fuster <vfustermartins@gmail.com>
2022-09-28 17:16:46 -0300

    creates IBOutlets BattleViewController

--- rickbad/rickbad/Controller/BattleViewController.swift
+++ rickbad/rickbad/Controller/BattleViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class BattleViewController: UIViewController {
     
+    // Outlets View Carregamento
+    @IBOutlet weak var iconLoading: UIImageView!
+    @IBOutlet weak var messageLoading: UILabel!
+    @IBOutlet weak var buttonBattle: UIButton!
+    
+    // Outlets View Batalha
+    @IBOutlet weak var buttonStartBattle: UIButton!
+    
+    @IBOutlet weak var viewFirstImage: UIView!
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var namePersonOne: UILabel!
+    
+    
+    @IBOutlet weak var viewSecondImage: UIView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var namePersonTwo: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
