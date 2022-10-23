bd52d0b6e7b4b55e81f5b042c06addd4cf8b50f1
Valeria Fuster <vfustermartins@gmail.com>
2022-10-23 19:39:25 -0300

    create outlets and contraints HistoryCell

---
+++ rickbad/rickbad/View/HistoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  HistoryCell.swift
+//  rickbad
+//
+//  Created by Valeria Fuster on 19/10/22.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+
+    
+    @IBOutlet weak var winnerImage: UIImageView!
+    @IBOutlet weak var winnerName: UILabel!
+    @IBOutlet weak var winnerDate: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
